62eb44139bfb74b93aa0ab2863c6ae14f4335489
kaio kampos <kaiocamposti@gmail.com>
2025-03-25 19:45:04 -0300

    Criando as anotações customizadas

---
+++ Modulo30/src/main/java/com/kaio/anotacao/ColunaTabela.java
@@ -0,0 +1,13 @@
+package com.kaio.anotacao;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ColunaTabela {
+
+    String dbName();
+
+    String setJavaName();
+}
